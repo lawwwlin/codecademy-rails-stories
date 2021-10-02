49dd5dc0f262e9143f9cb2471082b5b428a57c31
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-10-01 20:14:29 -0700

    Update signups_controller.rb

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,2 +1,19 @@
 class SignupsController < ApplicationController
+
+  def new
+    @new_signup = Signup.new
+  end
+
+  def create
+    @new_signup = Signup.new(signup_params)
+    if @new_signup.save
+      redirect_to root_path
+    end
+  end
+
+  private
+  def signup_params
+    params.require(:signup).permit(:firstname, :email)
+  end
+
 end
