0a82eed9661c2d3d61629779ac3cc1693199ad21
Lawrence Lin <80787904+lawwwlin@users.noreply.github.com>
2021-10-01 20:28:11 -0700

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get '/about' => 'about#about'
-  get '/contact' => 'signups#new'
-  post '/contact' => 'signups#new'
+  resources :signups
 end
